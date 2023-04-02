e359a56fe5bc7c811960d4d821449a55112578bd
Max Bilokur <bilmaxim@gmail.com>
2023-04-02 23:29:15 +0300

    added address on render to allowed hosts in settings

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'https://task-manager-m1jl.onrender.com'
+]
 
 INTERNAL_IPS = [
     '127.0.0.1',
